8e7ee5b6f7979968621673a1909e403e99a2256d
Xiaochen Xu <xiaochex@gmail.com>
2021-06-07 21:40:00 -0400

    commit tree 0c32b4eb9ece961be4b2c6571c0aec38da4b6ddd

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,17 @@
-require 'pry'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
@@ -19,24 +23,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
